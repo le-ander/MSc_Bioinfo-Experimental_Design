5bf0fa05a5d53daf14905c6932586ddabe1c1422
jonasmac16 <jonas.mackerodt16@imperial.ac.uk>
2017-02-24 12:28:36 +0000

    removed import of abcsysbio parsers

--- simulations/simulation_functions.py
+++ simulations/simulation_functions.py
@@ -7,8 +7,8 @@ import re
 
 import cudasim.Lsoda as Lsoda
 
-from abcsysbio import parse_infoEnt
-from abcsysbio_parser import ParseAndWrite
+#from abcsysbio import parse_infoEnt
+#from abcsysbio_parser import ParseAndWrite
 
 try:
 	import cPickle as pickle
