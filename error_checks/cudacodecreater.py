500057799958007882ec4fe100c3240108df6817
jonasmac16 <jonas.mackerodt16@imperial.ac.uk>
2017-02-27 14:05:56 +0000

    Resolved import error

--- error_checks/cudacodecreater.py
+++ error_checks/cudacodecreater.py
@@ -1,9 +1,9 @@
 import sys
-import ParseAndWrite
+import abcsysbio_parser.ParseAndWrite
 
 def cudacodecreater(input_files, inPath="",outPath=""):
 	intType = ["CUDA ODE"]*len(input_files)
 	names=[]
 	for i in range(0,len(input_files)):
 		names.extend(["Exp_"+repr(i+1)])
-	ParseAndWrite.ParseAndWrite(source=input_files,integrationType=intType,modelName=names,inputPath=inPath,outputPath=outPath,method=None)
+	abcsysbio_parser.ParseAndWrite.ParseAndWrite(source=input_files,integrationType=intType,modelName=names,inputPath=inPath,outputPath=outPath,method=None)
