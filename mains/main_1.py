b83c9aac92fd82b0b0d9c5958f3265893f9488d7
jonasmac16 <jonas.mackerodt16@imperial.ac.uk>
2017-02-27 13:56:00 +0000

    Removed sys.path for abcsysbio_parser

--- mains/main_1.py
+++ mains/main_1.py
@@ -5,7 +5,7 @@ import re
 sys.path.insert(1, '../error_checks')
 sys.path.insert(1, '../simulations')
 sys.path.insert(1, '../mutualInfo')
-sys.path.insert(1, '../abc-sysbio/abcsysbio_parser')
+#sys.path.insert(1, '../abc-sysbio/abcsysbio_parser')
 sys.path.insert(1, '../abc-sysbio')
 
 #sys.path.insert(1, '/cluster/home/saw112/work/Test_code/abcsysbio_parser') ## not sure what it does as directory doesn't exist
