61a6b1c677659fc6a5acec1a58c82d85e39774f1
jonasmac16 <jonas.mackerodt16@imperial.ac.uk>
2017-02-27 15:18:15 +0000

    Solved headless plottting problem and changed approach values

--- error_checks/plotbar.py
+++ error_checks/plotbar.py
@@ -1,4 +1,7 @@
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from numpy import *
 
 def plotbar(MutInfo, modelname, n_groups, approach):
 	index = arange(n_groups)
@@ -7,17 +10,17 @@ def plotbar(MutInfo, modelname, n_groups, approach):
 	opacity = 0.6
 
 	plt.bar(index, MutInfo, bar_width, alpha=opacity, align='center', color='black')
-	if approach==1:
-		plt.ylabel('I(Theta,X)', fontweight="bold")
-		plt.suptitle('Mutual Information All Parameter approach', fontweight="bold")
+	if approach==0:
+		plt.ylabel(r'I($\Theta$,X)', fontweight="bold")
+		plt.suptitle('Mutual Information - Prediction for all parameters', fontweight="bold")
+	elif approach==1:
+		plt.ylabel(r'I($\Theta$,X)', fontweight="bold")
+		plt.suptitle('Mutual Information - Prediction for subset of parameters')
 	elif approach==2:
-		plt.ylabel('I(Theta,X)')
-		plt.suptitle('Mutual Information Subset Parameter approach')
-	elif approach==3:
-		plt.ylabel('I(Y,X)')
-		plt.suptitle('Mutual Information Model Outcome')
+		plt.ylabel('I(Y,X)', fontweight="bold" )
+		plt.suptitle('Mutual Information - Prediction of Model Outcome')
 
-	plt.xlabel('Experiment')
+	plt.xlabel('Experiments', fontweight="bold")
 
 	plt.xticks(index, modelname)
 	if max(MutInfo)<100:
