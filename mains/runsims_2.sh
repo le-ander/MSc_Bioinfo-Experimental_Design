1f38e7064b5be44f3982aadeb842de9b9fd3d257
ld2113 <ld2113@ic.ac.uk>
2017-03-07 15:15:48 +0000

    Rename runsims scripts

--- mains/runsims_2.sh
+++
@@ -1,6 +0,0 @@
-module load python
-source activate /cluster/home/jm2716/work/conda_enviroments/Python27_GPU
-
-cd /cluster/home/ld2113/work/Experimental-Design/main
-
-python main_1.py -a 2 -i1 p53_model.xml p53_model_exp.xml -i2 data_p53 data_p53_exp -of=results -lc 00 -if=Example_data > log_repressilator.txt
