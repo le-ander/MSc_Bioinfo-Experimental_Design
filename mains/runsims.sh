8250e2c793b69990f5521bdd3423bf228f04055b
ld2113 <ld2113@ic.ac.uk>
2017-03-08 14:31:06 +0000

    Update runscript

--- mains/runsims.sh
+++ mains/runsims.sh
@@ -2,9 +2,10 @@
 
 module load python
 source activate /cluster/home/jm2716/work/conda_enviroments/Python27_GPU
+
 if [ $THISHOST == cuda10 ]
 then
-	export CUDA_DEVICE=1
+	export CUDA_DEVICE=3
 fi
 
 cd ~/work/Experimental-Design/mains
@@ -16,5 +17,8 @@ cd ~/work/Experimental-Design/mains
 #python main_1.py -a 1 -of=results -i1 input_file_hess1.xml -i2 new_file2 -lc 1 -if=Example_data
 
 ##gE3 (SBML input):
-python main_1.py -a 2 -i1 p53_model.xml p53_model_exp.xml -i2 data_p53 data_p53_exp -of=results -lc 00 -if=Example_data > ~/work/Testing/logfile_cuda09_48e5.txt
+python main_1.py -a 2 -i1 p53_model.xml p53_model_exp.xml -i2 data_p53 data_p53_exp -of=results -lc 00 -if=Example_data
+#> ~/work/Testing/logfile_cuda10_96e5.txt
+
+
 unset CUDA_DEVICE
