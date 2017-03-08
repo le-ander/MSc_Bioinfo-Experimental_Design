cf5dd1b775f1c4838880552bd58ba5dc0eebc2cb
ld2113 <ld2113@ic.ac.uk>
2017-03-08 13:42:31 +0000

    Add env variable to launch script for CUDA_DEVICE

--- mains/runsims.sh
+++ mains/runsims.sh
@@ -1,7 +1,13 @@
+#!/bin/sh
+
 module load python
 source activate /cluster/home/jm2716/work/conda_enviroments/Python27_GPU
+if [ $THISHOST == cuda10 ]
+then
+	export CUDA_DEVICE=1
+fi
 
-cd /cluster/home/ld2113/work/Experimental-Design/main
+cd ~/work/Experimental-Design/mains
 
 ##gE1 (SBML input):
 #python main_1.py -a 0 -of=results -i1 rep_test.xml -i2 new_file2 -lc 0 -if=Example_data
@@ -10,4 +16,5 @@ cd /cluster/home/ld2113/work/Experimental-Design/main
 #python main_1.py -a 1 -of=results -i1 input_file_hess1.xml -i2 new_file2 -lc 1 -if=Example_data
 
 ##gE3 (SBML input):
-python main_1.py -a 2 -i1 p53_model.xml p53_model_exp.xml -i2 data_p53 data_p53_exp -of=results -lc 00 -if=Example_data > log_repressilator.txt
+python main_1.py -a 2 -i1 p53_model.xml p53_model_exp.xml -i2 data_p53 data_p53_exp -of=results -lc 00 -if=Example_data > ~/work/Testing/logfile_cuda09_48e5.txt
+unset CUDA_DEVICE
