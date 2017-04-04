31bb26c7afb44c3d40c143513c3c43ef89ba2710
jonasmac16 <jonas.mackerodt16@imperial.ac.uk>
2017-04-04 11:50:37 +0100

    initilsid peitho package repo

---
+++ README.md
@@ -0,0 +1,44 @@
+# Experimental-Design ToDo
+
+## Ask Juliane
+
+- How to present repressilator results?
+- Should libsbml be a dependency or not?
+- Should we have a table with author contributions at the end of the report?
+
+## Next steps
+
+- Check for and filter nans (not just infs) (Jonas)
+
+- Peitho installation (Jonas)
+
+- Run timing runs
+
+- Group report
+  - Figure out how to present repressilator results (incl. results)
+  - Complete Troubleshooting (Jonas)
+  - Write Future Work
+
+- Read through group report
+
+- At very end: Final run through code (rm print statements, rm seeds) (Scott, Jonas, Leander) - remove Example data rubbish
+
+## On Hold
+
+- SDE implementation (Scott, Leander)
+
+- HDF5 to avoid memory issues (Jonas)
+
+- Pickling (reuse cudasim)  (Jonas)
+
+- Only load packages when required (libsbml and means) (wait for Julianes opinion)
+
+## Further work ideas
+
+- Improve CUDA-sim implementation
+  - Scott's for-loop to be pushed to new update
+  - Fix CUDAsim device detection
+
+- Convert to Python 3.5
+  - Modernize package
+  - Use Six package for compatibility
---
+++ peitho/Example/_results_/cudacodes/cudacodes_rep_test/Exp_3.cu
@@ -0,0 +1,27 @@
+#define NSPECIES 6
+#define NPARAM 5
+#define NREACT 12
+
+struct myFex{
+    __device__ void operator()(int *neq, double *t, double *y, double *ydot/*, void *otherData*/)
+    {
+        int tid = blockDim.x * blockIdx.x + threadIdx.x;
+
+
+
+        ydot[5]=(1.0*(tex2D(param_tex,4,tid)*y[4])-1.0*(tex2D(param_tex,4,tid)*y[5]))/tex2D(param_tex,0,tid);
+        ydot[4]=(1.0*(0.1*tex2D(param_tex,2,tid)+tex2D(param_tex,3,tid)/(1+__powf(y[3],tex2D(param_tex,1,tid))))-1.0*(y[4]))/tex2D(param_tex,0,tid);
+        ydot[3]=(1.0*(tex2D(param_tex,4,tid)*y[2])-1.0*(tex2D(param_tex,4,tid)*y[3]))/tex2D(param_tex,0,tid);
+        ydot[2]=(1.0*(0.1*tex2D(param_tex,2,tid)+0.2*tex2D(param_tex,3,tid)/(1+__powf(y[1],tex2D(param_tex,1,tid))))-1.0*(y[2]))/tex2D(param_tex,0,tid);
+        ydot[1]=(1.0*(tex2D(param_tex,4,tid)*y[0])-1.0*(tex2D(param_tex,4,tid)*y[1]))/tex2D(param_tex,0,tid);
+        ydot[0]=(1.0*(0.1*tex2D(param_tex,2,tid)+tex2D(param_tex,3,tid)/(1+__powf(y[5],tex2D(param_tex,1,tid))))-1.0*(y[0]))/tex2D(param_tex,0,tid);
+
+    }
+};
+
+
+ struct myJex{
+    __device__ void operator()(int *neq, double *t, double *y, int ml, int mu, double *pd, int nrowpd/*, void *otherData*/){
+        return; 
+    }
+};
