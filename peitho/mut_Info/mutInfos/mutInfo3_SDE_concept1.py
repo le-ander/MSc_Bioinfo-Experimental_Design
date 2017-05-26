101a10d5226e9d4d5b73d921a2b679ec3dd58e38
ld2113 <ld2113@ic.ac.uk>
2017-05-26 10:41:46 +0100

    WOrking on mutinfo3SDE

--- peitho/mut_Info/mutInfos/mutInfo3_SDE_concept1.py
+++ peitho/mut_Info/mutInfos/mutInfo3_SDE_concept1.py
@@ -14,16 +14,16 @@ random.seed(123)
 N1 = 43
 B_mod = 19
 B_ref = 19
-N3 = 37
+N4 = 37
 T = 33
 S = 13
 
 data_ref_t = random.rand(N1,B_ref,T,S).astype(float64)
-theta_ref_t = random.rand(N1+N3,T,S).astype(float64)
+theta_ref_t = random.rand(N1+N4,T,S).astype(float64)
 cov_ref_t = random.rand(N1+N4,S*T,S).astype(float64)
 data_mod_t = random.rand(N1,B_mod,T,S).astype(float64)
 theta_mod_t = random.rand(N1+N4,T,S).astype(float64)
-cov_mod_t = random.rand(N1+N3,S*T,S).astype(float64)
+cov_mod_t = random.rand(N1+N4,S*T,S).astype(float64)
 
 ##Check input types!!
 
@@ -52,18 +52,18 @@ def mutInfo1SDE(dataMod,thetaMod,covMod,dataRef,thetaRef,covRef):
 	}
 
 	//Function to calculate sum multivariate gaussians for second log term
-	__global__ void kernel_func1SDE(int n1, int b, int n3, double pre, float *invdet, double *x, double *mu, float *invcov, double *res1){
+	__global__ void kernel_func1SDE(int n1, int b, int n4, double pre, float *invdet, double *x, double *mu, float *invcov, double *res1){
 
 		unsigned int ti = threadIdx.x + blockDim.x * blockIdx.x;
 		unsigned int tj = threadIdx.y + blockDim.y * blockIdx.y;
 		unsigned int tk = threadIdx.z + blockDim.z * blockIdx.z;
 
-		if((ti>=n1)||(tj>=b)||(tk>=n3)) return;
+		if((ti>=n1)||(tj>=b)||(tk>=n4)) return;
 
 		double vector1[%(ST)s] = {0.0};
 		double vector2[%(T)s] = {0.0};
 
-		res1[idx3d(ti,tj,tk,b,n3)] = 0.0;
+		res1[idx3d(ti,tj,tk,b,n4)] = 0.0;
 
 		for(int t=0; t<%(T)s; t++){
 
@@ -77,15 +77,15 @@ def mutInfo1SDE(dataMod,thetaMod,covMod,dataRef,thetaRef,covRef):
 				}
 				vector2[t] += vector1[idx2d(t,s_i,%(S)s)] * (x[idx4d(ti,tj,t,s_i,b,%(T)s,%(S)s)] - mu[idx3d(tk,t,s_i,%(T)s,%(S)s)]);
 			}
-			res1[idx3d(ti,tj,tk,b,n3)] += log(sqrtf(invdet[idx2d(tk,t,%(T)s)])) - 0.5 * vector2[t] + pre;
+			res1[idx3d(ti,tj,tk,b,n4)] += log(sqrtf(invdet[idx2d(tk,t,%(T)s)])) - 0.5 * vector2[t] + pre;
 		}
-		res1[idx3d(ti,tj,tk,b,n3)] = exp(res1[idx3d(ti,tj,tk,b,n3)]);
+		res1[idx3d(ti,tj,tk,b,n4)] = exp(res1[idx3d(ti,tj,tk,b,n4)]);
 	}
 	"""
 
 	print "\n", "-----Preprocessing Data (matrix inversion etc.)-----", "\n"
 
-	# Determine number of particles (N1,N3), betas (B), timepoints (T), species (S)
+	# Determine number of particles (N1,N4), betas (B), timepoints (T), species (S)
 	N1, B_ref, T, S = data_ref.shape
 	B_mod = data_mod.shape[1]
 	N4 = theta.shape[0] - N1
@@ -107,14 +107,14 @@ def mutInfo1SDE(dataMod,thetaMod,covMod,dataRef,thetaRef,covRef):
 	pre = log(1/(sqrt(pow(2*math.pi,S))))
 
 	# Initialise arrays for inverted covariance matrices and inverted determinants
-	invcov = zeros((N1+N3,S*T,S), dtype=float32)
-	invdet = zeros((N1+N3,T), dtype=float32)
+	invcov = zeros((N4,S*T,S), dtype=float32)
+	invdet = zeros((N4,T), dtype=float32)
 
 	# Invert covariance matrices and calculate the determinant of the inverted matrices
 	##Remove abs() for invdet
-	for i in range(N1+N3):
+	for i in range(N4):
 		for j in range(T):
-			invcov[i,j*S:(j+1)*S,:] = linalg.inv(cov[i,j*S:(j+1)*S,:])
+			invcov[i,j*S:(j+1)*S,:] = linalg.inv(cov[N1+i,j*S:N1+(j+1)*S,:])
 			invdet[i,j] = abs(linalg.det(invcov[i,j*S:(j+1)*S,:]))
 
 	#################### Calculating second log term ###########################
@@ -136,8 +136,8 @@ def mutInfo1SDE(dataMod,thetaMod,covMod,dataRef,thetaRef,covRef):
 	# Launch configuration: Grid size (limited by GPU global memory) and grid shape (multipe of block size)
 	grid_prelim_i , grid_prelim_j, grid_prelim_k = launch.optimise_gridsize_sde(1, float(block_i), float(block_j), float(block_k), T, S)
 	grid_i = float(min(autoinit.device.max_grid_dim_x, grid_prelim_i, N1))
-	grid_j = float(min(autoinit.device.max_grid_dim_y, grid_prelim_j, B))
-	grid_k = float(min(autoinit.device.max_grid_dim_y, grid_prelim_k, N3))
+	grid_j = float(min(autoinit.device.max_grid_dim_y, grid_prelim_j, B_mod))
+	grid_k = float(min(autoinit.device.max_grid_dim_y, grid_prelim_k, N4))
 	print "Grid shape:", str(grid_i)+"x"+str(grid_j)+"x"+str(grid_k)
 	print "Registers:", gpu_kernel_func1SDE.num_regs , "\n"
 
@@ -145,11 +145,11 @@ def mutInfo1SDE(dataMod,thetaMod,covMod,dataRef,thetaRef,covRef):
 
 	# Determine required number of runs for i and j
 	numRuns_i = int(ceil(N1/grid_i))
-	numRuns_j = int(ceil(B/grid_j))
-	numRuns_k = int(ceil(N3/grid_k))
+	numRuns_j = int(ceil(B_mod/grid_j))
+	numRuns_k = int(ceil(N4/grid_k))
 
-	# Initialise array to hold results of log N3 avarage
-	res_log_2 = zeros([N1,B], dtype=float64)
+	# Initialise array to hold results of model probability
+	res_log_mod = zeros([N1,B_mod,N4], dtype=float64)
 
 	# Create template array for res1
 	try:
@@ -182,11 +182,11 @@ def mutInfo1SDE(dataMod,thetaMod,covMod,dataRef,thetaRef,covRef):
 		for j in range(numRuns_j):
 
 			# If last run with less that max remaining particles, set Ni to remaining number of particles
-			if((int(grid_j)*(j+1)) > B):
-				Nj = int(B - grid_j*j)
+			if((int(grid_j)*(j+1)) > B_mod):
+				Nj = int(B_mod - grid_j*j)
 
 			# Prepare data that depends on i and j for this run
-			data_subset = ascontiguousarray(data[(i*int(grid_i)):(i*int(grid_i)+Ni),(j*int(grid_j)):(j*int(grid_j)+Nj),:,:]) # d1 subunit for the next k runs
+			data_subset = ascontiguousarray(data[(i*int(grid_i)):(i*int(grid_i)+Ni),(j*int(grid_j)):(j*int(grid_j)+Nj),:,:])
 
 			# Set i dimension of block and grid for this run
 			if(Nj<block_j):
@@ -205,14 +205,14 @@ def mutInfo1SDE(dataMod,thetaMod,covMod,dataRef,thetaRef,covRef):
 			for k in range(numRuns_k):
 
 				# If last run with less that max remaining particles, set Nj to remaining number of particles
-				if((int(grid_k)*(k+1)) > N3):
-					Nk = int(N3 - grid_k*k)
+				if((int(grid_k)*(k+1)) > N4):
+					Nk = int(N4 - grid_k*k)
 					last = True
 
 				# Prepare input that depends on k for this run
 				theta_subset = theta[N1+(k*int(grid_k)):N1+(k*int(grid_k)+Nk),:,:]
-				invcov_subset = invcov[N1+(k*int(grid_k)):N1+(k*int(grid_k)+Nk),:,:]
-				invdet_subset = invdet[N1+(k*int(grid_k)):N1+(k*int(grid_k)+Nk),:]
+				invcov_subset = invcov[(k*int(grid_k)):(k*int(grid_k)+Nk),:,:]
+				invdet_subset = invdet[(k*int(grid_k)):(k*int(grid_k)+Nk),:]
 
 				# Set k dimension of block and grid for this run
 				if(Nk<block_k):
@@ -231,6 +231,17 @@ def mutInfo1SDE(dataMod,thetaMod,covMod,dataRef,thetaRef,covRef):
 				# Call GPU kernel function
 				gpu_kernel_func1SDE(int32(Ni), int32(Nj), int32(Nk), float64(pre), driver.In(invdet_subset), driver.In(data_subset),driver.In(theta_subset), driver.In(invcov_subset),driver.Out(res1), block=(int(bi),int(bj),int(bk)),grid=(int(gi),int(gj),int(gk)))
 
+
+
+
+
+
+
+
+
+
+
+
 				# Sum over N3 and sore in results array
 				res_log_2[i*int(grid_i):i*int(grid_i)+Ni,j*int(grid_j):j*int(grid_j)+Nj] += sum(res1, axis=2)
 
@@ -238,99 +249,6 @@ def mutInfo1SDE(dataMod,thetaMod,covMod,dataRef,thetaRef,covRef):
 	res_log_2 = log(res_log_2) - log(N3)
 
 
-	#################### Calculating first log term ###########################
-
-	print "-----Determining optimal kernel launch configuration (for part 2/2)-----"
-
-	# Launch configuration: Block size and shape (as close to square as possible)
-	block = launch.optimal_blocksize(autoinit.device, gpu_kernel_func2SDE)
-	block_i = launch.factor_partial(block)
-	block_j = block / block_i
-	print "Optimal blocksize:", block, "threads"
-	print "Block shape:", str(block_i)+"x"+str(block_j)
-
-
-	# Launch configuration: Grid size (limited by GPU global memory) and grid shape (multipe of block size)
-	grid_prelim_i , grid_prelim_j = launch.optimise_gridsize_sde(2, block_i, block_j, 0, T, S)[0:2]
-	grid_i = float(min(autoinit.device.max_grid_dim_x, grid_prelim_i, N1))
-	grid_j = float(min(autoinit.device.max_grid_dim_y, grid_prelim_j, B))
-	print "Grid shape:", str(grid_i)+"x"+str(grid_j)
-	print "Registers:", gpu_kernel_func2SDE.num_regs , "\n"
-
-	print "-----Calculation part 2 of 2 now running-----"
-
-	# Determine required number of runs for i and j
-	numRuns_i = int(ceil(N1/grid_i))
-	numRuns_j = int(ceil(B/grid_j))
-
-	# Initialise array to hold results of second log term
-	res_log_1 = zeros([N1,B], dtype=float64)
-
-	# Create template array for res1
-	try:
-		template_res1 = zeros([int(grid_i),int(grid_j)], dtype=float64)
-	except:
-		print "ERROR: Not enought memory (RAM) available to create array for GPU results. Reduce GPU grid size."
-		sys.exit()
-
-	# Maximum number of particles per run in i, j and k direction
-	Ni = int(grid_i)
-
-	# Main nested for-loop for calculation of second log term
-	for i in range(numRuns_i):
-
-		# If last run with less that max remaining particles, set Ni to remaining number of particles
-		if((int(grid_i)*(i+1)) > N1):
-			Ni = int(N1 - grid_i*i)
-
-		# Prepare input that depends on i for this run
-		theta_subset = theta[(i*int(grid_i)):(i*int(grid_i)+Ni),:,:]
-		invcov_subset = invcov[(i*int(grid_i)):(i*int(grid_i)+Ni),:,:]
-		invdet_subset = invdet[(i*int(grid_i)):(i*int(grid_i)+Ni),:]
-
-		# Set i dimension of block and grid for this run
-		if(Ni<block_i):
-			gi = 1
-			bi = Ni
-		else:
-			gi = ceil(Ni/block_i)
-			bi = block_i
-
-		# Maximum number of particles per run in j direction
-		Nj = int(grid_j)
-
-		# Reset last to "False"
-		last = False
-
-		for j in range(numRuns_j):
-
-			# If last run with less that max remaining particles, set Ni to remaining number of particles
-			if((int(grid_j)*(j+1)) > B):
-				Nj = int(B - grid_j*j)
-				last = True
-
-			# Prepare data that depends on i and j for this run
-			data_subset = ascontiguousarray(data[(i*int(grid_i)):(i*int(grid_i)+Ni),(j*int(grid_j)):(j*int(grid_j)+Nj),:,:]) # d1 subunit for the next k runs
-
-			# Set j dimension of block and grid for this run
-			if(Nj<block_j):
-				gj = 1
-				bj = Nj
-			else:
-				gj = ceil(Nj/block_j)
-				bj = block_j
-
-			# Prepare results array for run
-			if last == True:
-				res1 = copy.deepcopy(template_res1[:Ni,:Nj])
-			elif j == 0:
-				res1 = copy.deepcopy(template_res1[:Ni,:Nj])
-
-			# Call GPU kernel function
-			gpu_kernel_func2SDE(int32(Ni), int32(Nj), float64(pre), driver.In(invdet_subset), driver.In(data_subset),driver.In(theta_subset), driver.In(invcov_subset),driver.Out(res1), block=(int(bi),int(bj),int(1)),grid=(int(gi),int(gj),int(1)))
-
-			# Store in results array
-			res_log_1[i*int(grid_i):i*int(grid_i)+Ni,j*int(grid_j):j*int(grid_j)+Nj] = res1
 
 	# Calculate final result
 	masked_diff = ma.masked_invalid(res_log_1 - res_log_2)
