7f0d7d957d4d1b367dd19421117ef0608de64c80
jonasmac16 <jonas.mackerodt16@imperial.ac.uk>
2017-08-29 18:39:04 +0100

    getting SDE to run

--- peitho/__init__.py
+++ peitho/__init__.py
@@ -1,8 +1,8 @@
-__all__= ["errors_and_parsers", "main", "mut_Info", "simulations"]
+#__all__= ["errors_and_parsers", "main", "mut_Info", "simulations"]
 
-import peitho.errors_and_parsers
-import peitho.main
-import peitho.mut_Info
-import peitho.simulations
+#import peitho.errors_and_parsers
+#import peitho.main
+#import peitho.mut_Info
+#import peitho.simulations
 
 #from .main import main
